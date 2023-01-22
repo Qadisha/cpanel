7004065901a973174636bcfad6f3d0383587733b
Antonino Esposito <antonino.esposito@infocube.it>
2023-01-22 15:56:59 +0100

    Backup all accounts to Hetzner Storage Box

---
+++ backup-accounts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DOW=($date +%a)
+
+# Create Backup folder for a specific date
+ssh -p23 HETZNERUSERNAME@HETZNERUSERNAME.your-storagebox.de mkdir -p QD/Webservers/www-01/$DOW
+
+# Iterate Cpanel Accounts and make a backup
+cd /var/cpanel/users
+for user in *
+do
+	cd /home/$user
+	mkdir -p /home/$user/qdbackup
+	/scripts/pkgacct --compress $user qdbackup/
+	rsync --progress -e 'ssh -p23' qdbackup/* HETZNERUSERNAME@HETZNERUSERNAME.your-storagebox.de:/home/QD/Webservers/www-01/$DOW
+	rm -Rf qdbackup
+done
